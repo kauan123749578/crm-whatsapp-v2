8dda370ce2a9e990c32311505b2ec3b3d69a5311
kauan123749578 <kauan123749578@users.noreply.github.com>
2026-01-13 03:09:45 -0300

    fix: Remover arquivos dist/ do repositÃ³rio para forÃ§ar rebuild

--- apps/backend/dist/auth/decorators.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Roles = exports.Public = void 0;
-const common_1 = require("@nestjs/common");
-const Public = () => (0, common_1.SetMetadata)('isPublic', true);
-exports.Public = Public;
-const Roles = (...roles) => (0, common_1.SetMetadata)('roles', roles);
-exports.Roles = Roles;
-//# sourceMappingURL=decorators.js.map
